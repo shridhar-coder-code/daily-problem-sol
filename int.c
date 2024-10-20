26553115f91f5f77164e6eb35d4df73661aeabea
shridhar-coder-code <sridharkalasannavar5@gmail.com>
2024-10-20 10:33:03 +0530

    Add files via upload

---
+++ int.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int n, j;
+    printf("Enter the number to write the table of it: ");
+    scanf("%d", &j);
+    
+    for(n = 1; n <= 10; n++) {
+        printf("%d * %d = %d\n", j, n, j * n);
+    }
+    
+    return 0;
+}
