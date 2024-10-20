3b9391640240b019f428c782c8735d701dc73cca
shridhar-coder-code <sridharkalasannavar5@gmail.com>
2024-10-20 10:41:43 +0530

    Update int.c

--- int.c
+++ int.c
@@ -5,7 +5,15 @@ int main() {
     printf("Enter the number to write the table of it: ");
     scanf("%d", &j);
     
+    
     for(n = 1; n <= 10; n++) {
+        printf("%d * %d = %d\n", j, n, j * n);
+    }
+
+    printf("\n");  
+    
+  
+    for(n = 10; n >= 1; n--) {
         printf("%d * %d = %d\n", j, n, j * n);
     }
     
