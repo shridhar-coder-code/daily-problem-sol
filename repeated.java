2b3ec84356500b3a8ac224e8a2f919819b2db3f1
shridhar-coder-code <sridharkalasannavar5@gmail.com>
2024-11-14 20:07:22 +0530

    Update repeated.java

--- repeated.java
+++ repeated.java
@@ -11,20 +11,26 @@ public class repeated {
         for (i = 0; i < n; i++) {
             arr[i] = in.nextInt();
         }
-        int[] count = new int[n];
-
+        
+        int max = arr[0];
+        for (i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        int[] count = new int[max+1];
+        
         for (i = 0; i < n; i++) {
+             
             count[arr[i]]++;
         }
         
-        for(i=0;i<=n;i++){
-        if (count[i] >= 1) {
-            System.out.println("the number " + i + " is repeated " + count[i] + " times.");
-        }
-       
-    }
-    in.close();
-}
-}
-    
+        for (i = 0; i <= max; i++) {
+            if (count[i] >= 1) {
+                System.out.println("the number " + i + " is repeated " + count[i] + " times.");
+            }
 
+        }
+        in.close();
+    }
+}
