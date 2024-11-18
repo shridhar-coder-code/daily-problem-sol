766c9476e8a947b633de1e46d74620b2cc684bc2
shridhar-coder-code <sridharkalasannavar5@gmail.com>
2024-11-18 21:26:53 +0530

    Update rotate.java

--- rotate.java
+++ rotate.java
@@ -1,35 +1,38 @@
 import java.util.Scanner;
 
-public class rotate {
+public class Rotate {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("enter the array size");
+        System.out.println("Enter the array size:");
         int n = sc.nextInt();
-        System.out.println("enter the array elements:");
+
+        System.out.println("Enter the array elements:");
         int arr[] = new int[n];
-        int i;
-        for (i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println("enter the position from the last to rotate the array:");
+
+        System.out.println("Enter the position from the last to rotate the array:");
         int k = sc.nextInt();
+
         k = k % n;
+
         int temp[] = new int[n];
 
-        for ( i = 0; i < k; i++) {
-            temp[i] = arr[n+ i - k ];
+        for (int i = 0; i < k; i++) {
+            temp[i] = arr[n - k + i];
         }
 
-        for ( i = 0; i < n - k; i++) {
+        for (int i = 0; i < n - k; i++) {
             temp[k + i] = arr[i];
         }
 
         System.out.println("Array after rotating from " + k + " positions:");
-        for ( i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             System.out.print(temp[i] + " ");
         }
+
         sc.close();
     }
-
 }
